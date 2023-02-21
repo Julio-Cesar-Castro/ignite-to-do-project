a63d3a431be4ecf0ed29fec5e43ff6f8c3c80ae2
JulioCastro240902 <castroj@vinhedo.balluff.net>
2023-02-20 23:37:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+
+export function App() {
+
+  return (
+    <div>
+     <Header />
+     <div className="wrapper">
+
+     </div>
+    </div>
+  )
+}
+
